97b8eea760ea05172ddb7a12c784a455ceb6c18a
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2026-02-19 16:37:07 +0000

    feat: set scaffold background color for improved UI

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
+        scaffoldBackgroundColor: const Color(0xFFF5F7FB),
       ),
       home: const CreateEventScreen(),
     );
