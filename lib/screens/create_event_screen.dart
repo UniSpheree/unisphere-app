767c07074beddb4a847b5ca10c646f97c234bef6
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2026-02-19 16:15:46 +0000

    feat: add create event screen scaffold

---
+++ lib/screens/create_event_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateEventScreen extends StatelessWidget {
+  const CreateEventScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Create Event Screen')));
+  }
+}
