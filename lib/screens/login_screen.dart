bb0b39d5311bc06240df0fa63909ff66d7b2347a
Marinos Pierides <pieridesmarinos@gmail.com>
2026-03-02 10:29:32 +0000

    feat(auth): wire Forgot Password? button on LoginScreen to /forgot-password route

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -177,7 +177,10 @@ class _LoginScreenState extends State<LoginScreen> {
                     Align(
                       alignment: Alignment.centerRight,
                       child: TextButton(
-                        onPressed: () {},
+                        onPressed: () => Navigator.pushNamed(
+                          context,
+                          '/forgot-password',
+                        ),
                         style: TextButton.styleFrom(
                           padding: EdgeInsets.zero,
                           minimumSize: Size.zero,
