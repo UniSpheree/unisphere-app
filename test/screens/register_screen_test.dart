98f38ae1bb882fc99dbc011e2f26f5b306cdb631
Marinos Pierides <pieridesmarinos@gmail.com>
2026-05-05 00:59:46 +0100

    feat: add initial test for RegisterScreen import functionality

---
+++ test/screens/register_screen_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unisphere_app/screens/register_screen.dart';
+
+void main() {
+  test('imports register_screen', () {
+    expect(1, 1);
+  });
+}
