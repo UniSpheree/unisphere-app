26218ab63204f271f9e3aaba47f9a29c683ac67a
Marinos Pierides <pieridesmarinos@gmail.com>
2026-05-05 00:59:29 +0100

    feat: add initial test for PrivacyPage import functionality

---
+++ test/screens/privacy_page_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unisphere_app/screens/privacy_page.dart';
+
+void main() {
+  test('imports privacy_page', () {
+    expect(1, 1);
+  });
+}
