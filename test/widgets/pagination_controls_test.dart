a147cd57d265cf9ffa8b35a968edce40f1807f52
Marinos Pierides <pieridesmarinos@gmail.com>
2026-05-05 00:58:01 +0100

    feat: add initial test for pagination_controls widget

---
+++ test/widgets/pagination_controls_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unisphere_app/widgets/pagination_controls.dart';
+
+void main() {
+  test('imports pagination_controls', () {
+    expect(1, 1);
+  });
+}
