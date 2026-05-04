d797021c1acc0f0ce6779c303638c4a86451d663
Marinos Pierides <pieridesmarinos@gmail.com>
2026-05-05 00:59:15 +0100

    feat: add initial test for MyEventsPage import functionality

---
+++ test/screens/my_events_page_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unisphere_app/screens/my_events_page.dart';
+
+void main() {
+  test('imports my_events_page', () {
+    expect(1, 1);
+  });
+}
