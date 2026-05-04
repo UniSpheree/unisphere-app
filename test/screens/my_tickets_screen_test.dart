354fc5fee54c6bea8e4e6c89e53b1d90811a702e
Marinos Pierides <pieridesmarinos@gmail.com>
2026-05-05 00:59:23 +0100

    feat: add initial test for MyTicketsScreen import functionality

---
+++ test/screens/my_tickets_screen_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unisphere_app/screens/my_tickets_screen.dart';
+
+void main() {
+  test('imports my_tickets_screen', () {
+    expect(1, 1);
+  });
+}
