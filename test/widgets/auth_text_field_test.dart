87e66ccf8edc196b8ad7a0315d1fe3c93c7a99d9
Marinos Pierides <pieridesmarinos@gmail.com>
2026-05-05 01:02:35 +0100

    feat: add unit test for auth_text_field widget

---
+++ test/widgets/auth_text_field_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unisphere_app/widgets/auth_text_field.dart';
+
+void main() {
+  test('imports auth_text_field', () {
+    expect(1, 1);
+  });
+}
